f59293e246633bcc68cddf9dc40890e17dd723f5
Francisco Juan <francisco.juan@gmail.com>
2016-05-15 15:46:11 +0200

    reduce complexity on reset_pk_sequence method

--- lib/activerecord-reset-pk-sequence.rb
+++ lib/activerecord-reset-pk-sequence.rb
@@ -2,20 +2,30 @@ require 'activerecord-reset-pk-sequence/version'
 
 module ActiveRecord
   class Base
-    def self.reset_pk_sequence
-      case ActiveRecord::Base.connection.adapter_name
-      when 'SQLite'
-        new_max = maximum(primary_key) || 0
-        update_seq_sql = "UPDATE sqlite_sequence SET seq = #{new_max} WHERE name = '#{table_name}';"
-        ActiveRecord::Base.connection.execute(update_seq_sql)
-      when 'Mysql'
-        new_max = maximum(primary_key) + 1 || 1
-        update_seq_sql = "ALTER TABLE '#{table_name}' AUTO_INCREMENT = #{new_max};"
-        ActiveRecord::Base.connection.execute(update_seq_sql)
-      when 'PostgreSQL'
-        ActiveRecord::Base.connection.reset_pk_sequence!(table_name)
-      else
-        raise 'Task not implemented for this DB adapter'
+    class << self
+      def reset_pk_sequence
+        case ActiveRecord::Base.connection.adapter_name
+        when 'SQLite'
+          ActiveRecord::Base.connection.execute(sqlite_update_seq_sql)
+        when 'Mysql'
+          ActiveRecord::Base.connection.execute(mysql_update_seq_sql)
+        when 'PostgreSQL'
+          ActiveRecord::Base.connection.reset_pk_sequence!(table_name)
+        else
+          raise 'Task not implemented for this DB adapter'
+        end
+      end
+
+      private
+
+      def sqlite_update_seq_sql
+        new_max_id = maximum(primary_key) || 0
+        "UPDATE sqlite_sequence SET seq = #{new_max_id} WHERE name = '#{table_name}';"
+      end
+
+      def mysql_update_seq_sql
+        new_max_id = maximum(primary_key) + 1 || 1
+        "ALTER TABLE '#{table_name}' AUTO_INCREMENT = #{new_max_id};"
       end
     end
   end
